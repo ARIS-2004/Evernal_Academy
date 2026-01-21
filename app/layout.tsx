ae137cae9b342e292d6c28a63204bf62bb8279a8
Ramesh Naandi <rameshvai340@gmail.com>
2026-01-21 17:58:20 +0530

    9

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,17 +18,17 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/logo_crop.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/logo_crop.png",
         media: "(prefers-color-scheme: dark)",
       },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
+      // {
+      //   url: "/logo_2-removebg-preview.png",
+      //   type: "image/svg+xml",
+      // },
     ],
     apple: "/apple-icon.png",
   },
